5650135c15527905979fbab01de580f4826c433d
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2021-12-29 14:00:13 +0200

    Add HSL copyright

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ function IndexPage(props) {
     <main>
       <title>Timetables</title>
       <TimetableLoader {...props} />
+      <small>Data source: <a href="https://www.hsl.fi/en/hsl/open-data">© HSL 2016</a></small>
     </main>
   )
 }
